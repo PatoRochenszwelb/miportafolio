0585835b3a80bbbac16be589150b3b8a8cebad52
Patricio <p.rochenszwelb@gmail.com>
2020-06-24 08:38:30 -0400

    Agregado tooltip seccion mi experiencia

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$('#mi-experiencia [data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'top',
+    html: true
+});
+
+$('#mi-experiencia [data-toggle="tooltip-b"]').tooltip({
+    animated: 'fade',
+    placement: 'bottom',
+    html: true
+});
